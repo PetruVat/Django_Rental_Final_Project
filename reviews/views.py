18d65c0fb9a5113c4d93be100b56d68c4ddb2ea7
PetruVat <petruvatov@gmail.com>
2025-05-29 14:12:55 +0200

    add reviews/views

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from reviews.models import Review
+from reviews.serializers import ReviewSerializer
 
-# Create your views here.
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        # Просматривать можно все отзывы к конкретному объявлению
+        listing_id = self.request.query_params.get('listing')
+        if listing_id:
+            return self.queryset.filter(listing_id=listing_id)
+        return self.queryset
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
