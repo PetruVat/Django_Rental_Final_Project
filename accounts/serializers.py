07a5b89d4c397f48460f2b956ec15d5ad5f925a9
PetruVat <petruvatov@gmail.com>
2025-05-29 13:57:30 +0200

    add accounts/views

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,6 +3,12 @@ from accounts.models import User
 from django.contrib.auth.password_validation import validate_password
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'role')
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True,
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
 
-# Create your views here.
+from accounts.models import User
+from accounts.serializers import UserSerializer, RegisterSerializer
+
+
+# Регистрация
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+# Получение токена (login)
+class MyTokenObtainPairView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = TokenObtainPairView.serializer_class
+
+
+# Профиль текущего пользователя
+class UserDetailView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
